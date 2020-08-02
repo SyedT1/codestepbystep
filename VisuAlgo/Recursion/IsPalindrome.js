f2fbb6c5e14668b710235a8d46f47d0e6200fa20
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-02 11:23:02 +0600

    Custom Code
    Visualizing IsPaindrome code for Cpp and Js

---
+++ VisuAlgo/Recursion/IsPalindrome.js
@@ -0,0 +1,11 @@
+f(i="racecar"){
+  if(i.length==1) return true;
+  else if(i.charAt(0)==i.charAt(i.length-1)){
+  if(i.length==2) return true;
+  else{
+    var n = i.length-1;
+    return f(i.substr(1,n-1));
+  }
+ }
+ else return false;
+}
