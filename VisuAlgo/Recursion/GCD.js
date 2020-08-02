9c25521832b9b02330e1665719f83f33bcfbc91f
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-02 17:25:48 +0600

    GCD

--- VisuAlgo/Recursion/GCD.js
+++ VisuAlgo/Recursion/GCD.js
@@ -1,9 +1,7 @@
 function f(x = 2, y = 3){
     if(x==0||y==0) return 0;
     if(x<y){
-      var temp = x;
-      x  = y;
-      y = temp;
+      return f(y,x);
     }
     return x%y==0?y:f(y,x%y);
 }
