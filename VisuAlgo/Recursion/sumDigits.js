17613049e46a741bc292ffeaaa8f3cd4477cbcac
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-03 20:09:34 +0600

    Update sumDigits.js

--- VisuAlgo/Recursion/sumDigits.js
+++ VisuAlgo/Recursion/sumDigits.js
@@ -1,7 +1,7 @@
 function f(x=2345){
- if(x==0){
-  return 0;
- }else{
- return (x%10)+f((x* 205) >> 11); // 205/2048 is nearly the same as /10
- }
+   if(n==0){
+    return 0;
+   }else{
+       return (n%10)+f((n%10)?n-(n%10):n/10);
+   }
 }
