f2dd10e674a0d745496ebb84f4673aa3455505a3
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-03 20:31:56 +0600

    Is Palindrome

--- VisuAlgo/Recursion/IsPalindrome.js
+++ VisuAlgo/Recursion/IsPalindrome.js
@@ -3,8 +3,7 @@ function f(i="racecar"){
   else if(i.charAt(0)==i.charAt(i.length-1)){
   if(i.length==2) return true;
   else{
-    var n = i.length-1;
-    return f(i.substr(1,n-1));
+    return f(i.substr(1,i.length-2));
   }
  }
  else return false;
